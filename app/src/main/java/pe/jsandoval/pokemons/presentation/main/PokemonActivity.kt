1d8372a0958abb10ab7b1fc9c4b5fa4e9d0b38eb
Jhonatan Sandoval <55766471+JhonatanSandovalRavn@users.noreply.github.com>
2019-11-28 22:06:55 -0500

    Johto region as default

--- app/src/main/java/pe/jsandoval/pokemons/presentation/main/PokemonActivity.kt
+++ app/src/main/java/pe/jsandoval/pokemons/presentation/main/PokemonActivity.kt
@@ -47,7 +47,7 @@ class PokemonActivity : BaseActivity(), PokemonView {
             val regionSelected = regions[position]
             presenter.getPokemons(regionSelected)
         }
-        presenter.getPokemons(regions[1]) // default
+        presenter.getPokemons(regions[2]) // default
     }
 
     override fun loadPokemons(data: List<Pokemon>) {
